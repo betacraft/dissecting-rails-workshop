99fe31e0dbc0b1897da9aa3357fc6f989b52b2f1
Ankur <anx450z@gmail.com>
2023-04-20 18:42:13 +0530

    add new problem statement for deep transform keys

---
+++ active_support/problem_statements/problem_2.rb
@@ -0,0 +1,21 @@
+# Problem
+## Transform all keys into uppercase
+
+hash = { 'a' => 1, 'b' => { 'c' => 2, 'd' => 3 , 'e' => { 'f' => 4, 'g' => 5 } } }
+
+# solution 
+def deep_upper_case(hash, result = {})
+  hash.each_with_object({}) do |(key, value), result|
+    new_key = key.to_s.upcase
+    new_value = value.is_a?(Hash) ? deep_upper_case(value, result) : value
+    result[new_key] = new_value
+  end
+end
+
+p deep_upper_case(hash)
+
+## solution with active support
+require 'active_support/all'
+
+new_hash = hash.deep_transform_keys { |key| key.to_s.upcase }
+puts new_hash

098ad94e2a438ff6dbd56c19d770a3153ad12703
Animesh-Ghosh <animesh@betacraft.io>
2023-04-21 17:30:09 +0530

    Integrate @mathewt-p's code for using ERB templating
    Refactored inline HTML in UserMailer into an ERB template.

--- active_record/mailers/user_mailer.rb
+++ active_record/mailers/user_mailer.rb
@@ -1,35 +1,24 @@
 require 'action_mailer'
+require 'erb'
 
 class UserMailer < ActionMailer::Base
   default from: 'user@example.com'
 
   def notification_mail
-    user = params[:user]
-    subject = "Notification for #{user.name}"
-    mail to: user.email,
+    @user = params[:user]
+    subject = "Notification for #{@user.name}"
+    mail to: @user.email,
          subject:,
          content_type: 'text/html',
-         body: notification_mail_body(user)
+         body: notification_mail_body
   end
 
   private
 
   # @param [User] user
   # @return [String]
-  def notification_mail_body(user)
-    <<~HTML
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Notification Mail</title>
-      </head>
-      <body>
-        <h1>Notification Mail</h1>
-        <p>Notification mail for <a href="mailto:#{user.email}">#{user.name}</a> from <a href="mailto:user@example.com">user@example.com</p>
-      </body>
-      </html>
-    HTML
+  def notification_mail_body
+    template = ERB.new(File.read(File.join(File.basename(__dir__), 'user_mailer/notification_mail.erb')))
+    template.result binding
   end
 end

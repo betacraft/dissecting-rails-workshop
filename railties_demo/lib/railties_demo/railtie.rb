ea7f5f5146625c61a9d33cf8a96478d51fea9db0
harunkumars <harun@betacraft.io>
2023-04-24 14:54:17 +0530

    use railtie to autoinclude our middleware when an application uses our gem

--- railties_demo/lib/railties_demo/railtie.rb
+++ railties_demo/lib/railties_demo/railtie.rb
@@ -1,4 +1,7 @@
 module RailtiesDemo
   class Railtie < ::Rails::Railtie
+    initializer "railties_demo.configure_rails_initialization" do |app|
+      app.middleware.use RailtiesDemo::SnakamelMiddleware
+    end
   end
 end

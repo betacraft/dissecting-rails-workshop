c24fd227b947673faf647f3111cd13b71ba20ace
harunkumars <harun@betacraft.io>
2023-04-24 14:13:25 +0530

    add demo controller code

--- railties_demo/test/dummy/app/controllers/demo_controller.rb
+++ railties_demo/test/dummy/app/controllers/demo_controller.rb
@@ -1,4 +1,40 @@
 class DemoController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def sample
+    puts "Request params as seen in App code: \n #{request.params}"
+    puts "Response as seen in App code: \n #{api_response}"
+    render json: api_response
+  end
+
+  private
+
+  def api_response
+    if approved_user?
+      {
+        success: true,
+        response_data: {
+          approval_status: 'approved'
+        }
+      }.to_json
+    else
+      {
+        success: true, #API returns success even if the user is not approved
+        response_data: {
+          approval_status: 'declined'
+        }
+      }.to_json
+    end
+  end
+
+  def approved_user?
+    first_name == 'ratnadeep'
+  end
+
+  def first_name
+    user_params[:first_name]&.downcase
+  end
+
+  def user_params
+    params.require(:demo).permit(:first_name)
   end
 end

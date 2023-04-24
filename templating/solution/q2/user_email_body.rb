28eae2e1b4eadb3115ca6b34367fe1ca5522105a
Mathew Thomas <mathew1.9thomas@gmail.com>
2023-04-24 12:55:25 +0530

    Review changes

--- templating/solution/q2/user_email_body.rb
+++ templating/solution/q2/user_email_body.rb
@@ -1,13 +1,26 @@
 require 'erb'
 
 class UserEmailBody
-	User = Struct.new(:name, :email)
-	def initialize(user = nil )
-		@user = user || User.new("John Snow", "king_in_the_north@house_stark.com")
-		@template = ERB.new(File.read("#{__dir__}/user_email_template.erb"))
-	end
+  User = Struct.new(:name, :email)
+  def initialize(user = nil )
+    @user = user || User.new("John Snow", "king_in_the_north@house_stark.com")
+    @template_string = File.read("#{__dir__}/user_email_template.erb")
+  end
 
-	def call
-		@template.result(binding)
-	end
+  def call
+    TemplateRenderer.new(binding).render
+  end
 end
+
+class TemplateRenderer
+  def initialize(obj)
+    @user = eval("@user", obj)
+    @template = ERB.new(eval("@template_string", obj))
+  end
+
+  def render
+    @template.result(binding)
+  end
+
+end
+p UserEmailBody.new.call
